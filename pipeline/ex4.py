21e859fb65430d908d3efecf533ca626e1d7c9b6
nooreenf16 <83175847+nooreenf16@users.noreply.github.com>
2022-08-11 18:26:47 +0530

    Update ex4.py

--- pipeline/ex4.py
+++ pipeline/ex4.py
@@ -1,47 +1,5 @@
-# Introduction to Data Science with Python
-# MTA - Spring 2021-2022.
-# Final Home Exercise.
-
-# ID of  student:
-# First and Last Names of student:
-
-
-# In this exercise you should implement a classification pipeline which aim at predicting the amount of hours
+# implement a classification pipeline which aim at predicting the amount of hours
 # a worker will be absent from work based on the worker characteristics and the work day missed.
-# Download the dataset from the course website, which is provided as a .csv file. The target label is 'TimeOff'.
-# You are free to use as many loops as you like, and any library functions from numpy, pandas and sklearn, etc...
-#
-# You should implement the body of the functions below. The main two points of entry to your code are DataPreprocessor class and
-# the train_model function. In the '__main__' section you are provided with an example of how your submission will be evaluated.
-# You are free to change the body of the functions and classes as you like - as long as it adheres to the provided input & output structure.
-# In all methods and functions the input structure and the required returned variables are explicitly stated.
-# Note that in order to evaluate the generalization error, you'll need to run cross validation as we demonstrated in classs,
-# However!!! In the final sunbmission we your file needs to contain only the methods of DataPreprocessor and the train_model function. Your
-# submision will be retrained on all the train dataset.
-# You are encouraged to run gridsearch to find the best model and hyper parameters as demonstrated in the previous exercise and class.
-#
-# To make thigs clear: you need to experiment with the preprocessing stage and the final model that will be used to fit. To get the
-# sense of how your model performs, you'll need to apply the CV approach and, quite possibly, do a grid search of the meta parameters.
-# In the end, when you think that you've achieved your best, you should make a clean - and runnable!!! - version of your insights,
-# which must adhere to the api provided below. Needless to say, it's better to work with the API from the get-go (from the start), to avoid
-# unnecessary bugs. In the evaluation stage, your code will be run once for training on *all* the train data, and then run once on the test data.
-#
-# You are expoected to get results between 50% and 100% accuracy on the test set.
-# Of course, the test set is not provided to you. Hhowever, as previously mentioned, running cross validation
-# (with enough folds) will give you a good estimation of the accuracy.
-#
-# Important: obtaining accuracy less than 50%, will grant you 65 points for this exercise.
-# Obtaining accuracy score above 50% will grant you 75 points minimum, however, your final score
-# will be according to the distribution of all submissions. Therefore, due to the competition nature of this exercise,
-# you may use any method or library that will grant you the highest score, even if not learned in class.
-#
-# Identical or equivalent submissions will give rise to a suspicion of plagiarism.
-#
-# Final submission instruction: in addition to stating your names and ID numbers in the body of this file, name the file in the following way:
-#
-# ex4_FirstName_LastName.py
-#
-# where FirstName, ... stand, naturally, for your name(s)
 
 
 from xml.etree.ElementInclude import include
